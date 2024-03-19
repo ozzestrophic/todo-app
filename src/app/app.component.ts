2395091372233707fb9eeec02719980b762d0bb6
Ozzeek <osama.m.alnagar@gmail.com>
2024-03-19 23:49:27 +0200

    feat: added item component, edit and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Item } from './item';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -34,5 +36,9 @@ export class AppComponent {
       description,
       done: false,
     });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
   }
 }
