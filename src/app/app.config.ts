cf37269f8479b7610bd3a5d4aa1f45650b8236bd
Ozzeek <osama.m.alnagar@gmail.com>
2024-03-20 09:45:36 +0200

    build: initialize firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDuE25-X1Zp7EDtpHoscJeGxKImGa_cfiI',
+  authDomain: 'todo-app-7daab.firebaseapp.com',
+  projectId: 'todo-app-7daab',
+  storageBucket: 'todo-app-7daab.appspot.com',
+  messagingSenderId: '41335064162',
+  appId: '1:41335064162:web:3bf90184e205211216f33b',
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
